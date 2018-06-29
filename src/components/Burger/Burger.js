5bd1495c2842b754f83d3da2d7d3122b7a420e06
jon <manojam79@gmail.com>
2018-06-29 21:59:54 +1000

    create burgers dynammically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,17 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+    const transformerdIngredients = Object.keys(props.ingredients).map((ingredientkey) => {
+        return [...Array(props.ingredients[ingredientkey])].map((val, index) => {
+            return <BurgerIngredient key={index} type={ingredientkey}></BurgerIngredient>;
+        });
+    });
     return (
         <div className={classes.burger}>
             <BurgerIngredient type="bread-top"></BurgerIngredient>
-            <BurgerIngredient type="cheese"></BurgerIngredient>
-            <BurgerIngredient type="meat"></BurgerIngredient>
+            {/*<BurgerIngredient type="cheese"></BurgerIngredient>
+            <BurgerIngredient type="meat"></BurgerIngredient>*/}
+            {transformerdIngredients}
             <BurgerIngredient type="bread-bottom"></BurgerIngredient>
         </div>
     );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,11 +4,21 @@ import Aux from '../../hoc/Auxi'
 import Burger from '../../components/Burger/Burger'
 
 class BurgerBuilder extends Component {
+
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 2,
+            cheese: 2,
+            meat: 2
+        }
+    }
+
     render() {
         return (
             <Aux>
                 <div>
-                    <Burger></Burger>
+                    <Burger ingredients={this.state.ingredients}></Burger>
                 </div>
                 <div>
                     Build Controls
